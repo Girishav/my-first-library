ab7bcba430edd1e6f639a69abea947457e2c236c
Girishav <girishavilam06@gmail.com>
2021-04-26 08:49:28 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
-import React from 'react'
-import calendarRequest from "./calendarRequest"
 
-class ExampleComponent {
-  constructor(){
-    this.calendar = null;
-  }
+import { NativeModules } from 'react-native';
+const { RNTestLibrary } = NativeModules;
+import ExampleComponent from "./ExampleComponent"
 
-  getCalendarList(name){ 
-
-    var self = this;
-    return new Promise((success, failure) => {
-      self.calendar = new calendarRequest();
-      self.calendar.getCalendarList(name).then(response => {
-        console.log("girish sdk data",response)
-        success(response);
-      }).catch(error => {
-        console.log("config sdk failed ::", error);
-        failure(error);
-      });
-    });
- 
-  }
-} 
-
-export const ExampleComp = new ExampleComponent();
+export {ExampleComponent}

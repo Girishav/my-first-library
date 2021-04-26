c6cd906af326dbc470a376e2e678daf12a34b25c
Girishav <girishavilam06@gmail.com>
2021-04-26 16:46:56 +0530

    changes

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.web.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: {
+           name: 'TestLibrary',
+           type: 'umd',
+          },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  mode: "development"
+}
